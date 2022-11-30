d87b62d2edca15fcd7b0976f4432534c88fe8d8c
Dallyn <dc22601@ms.wasatch.edu>
2022-11-30 10:15:18 -0700

    Shipping website done

---
+++ functions.js
@@ -0,0 +1,12 @@
+
+function putToast(){
+
+    window.alert("look outside your window");
+}
+
+function stylistChange(){
+
+ var textBox = document.getElementById("something");
+    textBox.style.backgroundColor = "red"
+
+}
